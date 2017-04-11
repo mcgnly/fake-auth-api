f708825fc5a49e6d577b06a2b317e16b7ef54d71
Katie McGinley <katie@mcgnly.com>
2017-04-11 17:59:52 +0200

    added a bunch of paths

--- index.js
+++ index.js
@@ -9,12 +9,31 @@ const app = new Koa();
 const router = new Router();
 
 router
-  .post('/oauth2/signup', function (ctx, next) {
-    ctx.body = 'Hello World!';
-  })
-  .get('/oauth2/signup', function (ctx, next) {
-    ctx.body = 'Hello World!';
-  });
+	.post('/oauth2/signup', function (ctx, next) {
+		ctx.body = 'you posted your form to a really real DB';
+	})
+	.get('/oauth2/signup', function (ctx, next) {
+		ctx.body = 'Hello World!';
+	})
+	.get('/internal/health', function (ctx, next) {
+		ctx.body = 'super healthy';
+	})
+	.post('/oauth2/change-password', function (ctx, next) {
+		ctx.body = 'you changed the password';
+	})
+	.post('/oauth2/auth', function (ctx, next) {
+		ctx.body = 'you posted the login form';
+	})
+	.get('oauth2/confirm', function (ctx, next) {
+		ctx.body = 'totally signed up';
+	})
+	.get('oauth2/reset-password', function (ctx, next) {
+		ctx.body = 'now you get an auth token to reset your password';
+	})
+	.post('oauth2/reset-password', function (ctx, next) {
+		ctx.body = 'you fake reset your password';
+	})
+  ;
 
 app.use(router.routes());
 
